f12c7eaae87d1ed4333d064c1ba3e859b8e4afee
Alban <albanbleicher>
2020-10-16 11:59:28 +0200

    add contact form

--- src/components/SessionsPage.vue
+++ src/components/SessionsPage.vue
@@ -2,6 +2,18 @@
     <div class="sessions">
         <h1>{{data.title}}</h1>
         <div v-html="html_content"></div>
+
+        <div class="tarifs">
+            <div v-for='item in data.tarifs' :key='item.sessions_tarif_name' class="tarif-element">
+                <div class="tarif-header">
+                    <h3>{{item.sessions_tarif_name}}</h3>
+                <p class="price">{{item.sessions_tarif_price}}€</p>
+
+                </div>
+                <p>{{item.sessions_tarif_desc}}</p>
+                <a href="#">Réserver cette prestation</a>
+            </div>
+        </div>
     </div>
 </template>
 <script>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+
+<div class="contact">
+    <h1>Contact</h1>
+    <form name="premier-contact" netlify>
+        <div class="group-input"><input type="text" placeholder="Prénom"><input type="text" placeholder="Nom"></div>
+        <div class="group-input"><input type="mail" placeholder="Mail"><input type="phone" placeholder="Téléphone"></div>
+        <textarea placeholder="Votre message"></textarea>
+        <button>Envoyer</button>
+    </form>
+</div>
+  </Layout>
+</template>
